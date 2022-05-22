8b1e66e562d3993842f6e7595ac15b0590c92200
Christopher Deodat <deodat5500@gmail.com>
2022-05-22 11:30:51 +0700

    remap pgup pgdn home end

--- keyboards/mode/m65ha_alpha/keymaps/trosfy/keymap.c
+++ keyboards/mode/m65ha_alpha/keymaps/trosfy/keymap.c
@@ -51,10 +51,10 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [BASE] = LAYOUT_all( /* Base */
-        TD(GRV_TAP_DANCE) , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_PGUP ,
-        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN,
-        TD(ESC_TAP_DANCE), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT , KC_HOME,
-	KC_LSFT,          KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , KC_END ,
+        TD(GRV_TAP_DANCE) , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_HOME ,
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
+        TD(ESC_TAP_DANCE), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT , KC_PGUP,
+	KC_LSFT,          KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , KC_PGDN ,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC ,                            KC_RALT, MO(FN2)  , KC_LEFT, KC_DOWN, KC_RGHT 
 	),
    [MAC] = LAYOUT_all(
@@ -237,10 +237,10 @@ void matrix_scan_user(void) {
     //   SEND_STRING("https://start.duckduckgo.com\n");
     // }
 
-    SEQ_TWO_KEYS(KC_PGDN, KC_HOME) {
+    SEQ_TWO_KEYS(KC_END, KC_PGUP) {
       register_code(KC_SLEP);
     }
-    SEQ_FOUR_KEYS(KC_PGDN, KC_HOME, KC_END, KC_RGHT) {
+    SEQ_FOUR_KEYS(KC_END, KC_PGUP, KC_PGDN, KC_RGHT) {
       register_code(KC_PWR);
     }
   }
