31027769614ab5aaee8e321016b757ffe2f168c4
Christopher Deodat <christopher.deodat@traveloka.com>
2022-09-13 14:00:32 +0700

    init with mode65 keycodes

--- keyboards/wilba_tech/rama_works_m65_b/rules.mk
+++ keyboards/wilba_tech/rama_works_m65_b/rules.mk
@@ -8,12 +8,15 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
+TAP_DANCE_ENABLE = yes
+LEADER_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
 EXTRAKEY_ENABLE = yes      # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 CIE1931_CURVE = yes
